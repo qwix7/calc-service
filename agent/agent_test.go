05acbf27d78609a19e887a7fa95cc7b001e9a0f5
qwix7 <semen@gordeev.org>
2025-02-28 15:14:53 +0300

    Update agent_test.go

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -8,9 +8,7 @@ import (
 	"testing"
 )
 
-// Функция для фейковой отправки результата
 func sendResult(taskID, result string) {
-	// Этот фейковый сервер будет имитировать оркестратор
 	http.HandleFunc("/internal/task/"+taskID, func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
@@ -18,31 +16,26 @@ func sendResult(taskID, result string) {
 	server := httptest.NewServer(http.DefaultServeMux)
 	defer server.Close()
 
-	// Отправляем результат
 	resp, err := http.Post(server.URL+"/internal/task/"+taskID, "application/json", bytes.NewBufferString(result))
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
-	// Проверяем, что ответ успешный
 	if resp.StatusCode != http.StatusOK {
 		t.Fatalf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
 	}
 }
 
 func TestFetchTask(t *testing.T) {
-	// Эмулируем ответ оркестратора
 	task := Task{
 		Expression: "2+2*2",
 	}
 
-	// Создаем фейковый сервер оркестратора
 	tt := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(task)
 	}))
 	defer tt.Close()
 
-	// Имитация получения задачи агентом
 	got, err := fetchTask(tt.URL)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
